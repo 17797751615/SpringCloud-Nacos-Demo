98fadc2955b7774c717e97ec3084269b6907fab2
镇四海 <17797751615@163.com>
2019-12-25 16:43:56 +0800

    nacos之服务注册发现

---
+++ threeday/src/main/java/com.zyk.three/ThreeMain.java
@@ -0,0 +1,16 @@
+package com.zyk.three;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author zyk
+ * @description
+ * @date 2019-12-25 15:27
+ */
+@SpringBootApplication
+public class ThreeMain {
+    public static void main(String[] args) {
+        SpringApplication.run(ThreeMain.class,args);
+    }
+}
---
+++ threeday/src/main/java/com/zyk/three/controller/DiscoveryController.java
@@ -0,0 +1,32 @@
+package com.zyk.three.controller;
+
+/**
+ * @author zyk
+ * @description
+ * @date 2019-12-25 15:44
+ */
+
+import com.alibaba.nacos.api.annotation.NacosInjected;
+import com.alibaba.nacos.api.exception.NacosException;
+import com.alibaba.nacos.api.naming.NamingService;
+import com.alibaba.nacos.api.naming.pojo.Instance;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("discovery")
+public class DiscoveryController {
+
+    @NacosInjected
+    private NamingService namingService;
+
+
+    @GetMapping(value = "/get")
+    public List<Instance> get(@RequestParam String serviceName) throws NacosException {
+        return namingService.getAllInstances(serviceName);
+    }
+}
