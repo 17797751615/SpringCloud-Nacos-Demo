00b05f6d4d5dc6170817c8311fda4a88861576ea
zouyunke <zou_yunke@hismartinfo.com>
2020-06-02 15:38:33 +0800

    add prod dev file.dir

--- hs_file-pro/src/main/java/com/hs/file/controller/FileUploadController.java
+++ hs_file-pro/src/main/java/com/hs/file/controller/FileUploadController.java
@@ -1,5 +1,8 @@
 package com.hs.file.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * @author Think
  * @Classname FileUploadController
@@ -24,5 +27,8 @@ package com.hs.file.controller;
  * ┃ ┫ ┫   ┃ ┫ ┫
  * ┗━┻━┛   ┗━┻━┛
  */
+@RestController
+@RequestMapping("/api/v1.0/file")
 public class FileUploadController {
+
 }
