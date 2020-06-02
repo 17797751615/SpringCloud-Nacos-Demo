0c21c42b0ed860e50d0dbd86f5c9cc6d85585510
zouyunke <zou_yunke@hismartinfo.com>
2020-06-02 15:32:18 +0800

    fix fileutils

--- hs_file-pro/src/main/java/com/hs/file/controller/utils/HsFileUtils.java
+++ hs_file-pro/src/main/java/com/hs/file/controller/utils/HsFileUtils.java
@@ -1,5 +1,16 @@
 package com.hs.file.controller.utils;
 
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.text.DateFormat;
+import java.time.LocalDateTime;
+import java.util.Date;
+import java.util.UUID;
+
 /**
  * @author Think
  * @Classname HsFileUtils
@@ -25,4 +36,25 @@ package com.hs.file.controller.utils;
  * ┗━┻━┛   ┗━┻━┛
  */
 public class HsFileUtils {
+    public String saveFile(String filePath,MultipartFile file) throws IOException {
+        StringBuilder builder = new StringBuilder();
+        builder.append(getTenTime()).append(".").append(file.getOriginalFilename());
+
+        String fileName = "/data/hipay/java/health/healthImages/"+ "UUidName";
+        byte[] bytes = file.getBytes();
+        Path laking = Paths.get(fileName);
+        Files.write(laking, bytes);
+        return "";
+    }
+    /**
+     * 得到十位时间戳
+     * @return
+     */
+    private String getTenTime() {
+        LocalDateTime localDateTime = LocalDateTime.now();
+        StringBuilder sb = new StringBuilder();
+        sb.append(localDateTime.getYear()-2000).append(localDateTime.getMonthValue())
+        .append(localDateTime.getDayOfMonth()).append(localDateTime.getHour()).append(localDateTime.getMinute());
+        return sb.toString();
+    }
 }
