b6a560c8ab88d0f1fb0f625771e3e95110fd2a68
zouyunke <zou_yunke@hismartinfo.com>
2020-06-02 14:16:49 +0800

    添加依赖和controller

---
+++ hs_file-pro/src/main/java/com/hs/file/controller/FileUploadController.java
@@ -0,0 +1,28 @@
+package com.hs.file.controller;
+
+/**
+ * @author Think
+ * @Classname FileUploadController
+ * @Description 文件上传入口
+ * @Date 2020/6/2 14:16
+ * @Created by Forrest
+ * @Email comeonZYK@outlook.com
+ * ┏┛ ┻━━━━━┛ ┻┓
+ * ┃　　　　　　 ┃
+ * ┃　　　━　　　┃
+ * ┃　┳┛　  ┗┳　┃
+ * ┃　　　　　　 ┃
+ * ┃　　　┻　　　┃
+ * ┃　　　　　　 ┃
+ * ┗━┓　　　┏━━━┛
+ * ┃　　　┃   神兽保佑
+ * ┃　　　┃   代码无BUG！
+ * ┃　　　┗━━━━━━━━━┓
+ * ┃　　　　　　　    ┣┓
+ * ┃　　　　         ┏┛
+ * ┗━┓ ┓ ┏━━━┳ ┓ ┏━┛
+ * ┃ ┫ ┫   ┃ ┫ ┫
+ * ┗━┻━┛   ┗━┻━┛
+ */
+public class FileUploadController {
+}
