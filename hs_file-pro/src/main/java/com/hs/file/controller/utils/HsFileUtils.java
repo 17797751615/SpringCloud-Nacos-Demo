f2705ad23a7cb17ca45cad105f1a818361bd9dd2
zouyunke <zou_yunke@hismartinfo.com>
2020-06-02 14:22:04 +0800

    add file fileutils

---
+++ hs_file-pro/src/main/java/com/hs/file/controller/utils/HsFileUtils.java
@@ -0,0 +1,27 @@
+package com.hs.file.controller.utils;
+
+/**
+ * @Classname HsFileUtils
+ * @Description TODO
+ * @Date 2020/6/2 14:21
+ * @Created by Forrest
+ * @Email comeonZYK@outlook.com
+ * ┏┛ ┻━━━━━┛ ┻┓
+ * ┃　　　　　　 ┃
+ * ┃　　　━　　　┃
+ * ┃　┳┛　  ┗┳　┃
+ * ┃　　　　　　 ┃
+ * ┃　　　┻　　　┃
+ * ┃　　　　　　 ┃
+ * ┗━┓　　　┏━━━┛
+ * ┃　　　┃   神兽保佑
+ * ┃　　　┃   代码无BUG！
+ * ┃　　　┗━━━━━━━━━┓
+ * ┃　　　　　　　    ┣┓
+ * ┃　　　　         ┏┛
+ * ┗━┓ ┓ ┏━━━┳ ┓ ┏━┛
+ * ┃ ┫ ┫   ┃ ┫ ┫
+ * ┗━┻━┛   ┗━┻━┛
+ */
+public class HsFileUtils {
+}
