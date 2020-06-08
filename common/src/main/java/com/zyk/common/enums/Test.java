5d990ebae85860570dedd27d9dacd27930970c1e
zouyunke <zou_yunke@hismartinfo.com>
2020-06-08 16:03:25 +0800

    delete idea

---
+++ common/src/main/java/com/zyk/common/enums/Test.java
@@ -0,0 +1,33 @@
+package com.zyk.common.enums;
+
+/**
+ * @Classname Test
+ * @Description TODO
+ * @Date 2020/6/2 11:26
+ * @Created by Forrest
+ * @Email comeonZYK@outlook.com
+ * ┏┛ ┻━━━━━┛ ┻┓
+ * ┃　　　　　　 ┃
+ * ┃　　　━　　　┃
+ * ┃　┳┛　  ┗┳　┃
+ * ┃　　　　　　 ┃
+ * ┃　　　┻　　　┃
+ * ┃　　　　　　 ┃
+ * ┗━┓　　　┏━━━┛
+ * ┃　　　┃   神兽保佑
+ * ┃　　　┃   代码无BUG！
+ * ┃　　　┗━━━━━━━━━┓
+ * ┃　　　　　　　    ┣┓
+ * ┃　　　　         ┏┛
+ * ┗━┓ ┓ ┏━━━┳ ┓ ┏━┛
+ * ┃ ┫ ┫   ┃ ┫ ┫
+ * ┗━┻━┛   ┗━┻━┛
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println(ResponseValues.SUCCESS);
+
+
+
+    }
+}
--- hs_file-pro/src/main/java/com/hs/file/controller/FileUploadController.java
+++ hs_file-pro/src/main/java/com/hs/file/controller/FileUploadController.java
@@ -2,12 +2,12 @@ package com.hs.file.controller;
 
 import com.hs.file.utils.HsFileUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.IOException;
+import javax.activation.MimetypesFileTypeMap;
+import javax.servlet.http.HttpServletResponse;
+import java.io.*;
 
 /**
  * @author Think
@@ -43,5 +43,29 @@ public class FileUploadController {
     public String saveFile(MultipartFile file) throws IOException {
         String fileName = hsFileUtils.saveFile(file);
         return fileName;
+    }
+    @GetMapping("/{fileName}")
+    public void getFile(@PathVariable String fileName, HttpServletResponse response) throws IOException {
+
+        // 设置信息给客户端不解析
+        String type = new MimetypesFileTypeMap().getContentType(fileName);
+        // 设置contenttype，即告诉客户端所发送的数据属于什么类型
+        response.setContentType("application/x-png");
+        // 设置编码
+        String hehe = new String(fileName.getBytes("utf-8"), "iso-8859-1");
+        // 设置扩展头，当Content-Type 的类型为要下载的类型时 , 这个信息头会告诉浏览器这个文件的名字和类型。
+//        response.setHeader("Content-Disposition", "attachment;filename=" + hehe);
+        // 发送给客户端的数据
+        OutputStream outputStream = response.getOutputStream();
+        byte[] buff = new byte[1024];
+        BufferedInputStream bis = null;
+        // 读取filename
+        bis = new BufferedInputStream(new FileInputStream(new File("D:/test/file/" + fileName)));
+        int i = bis.read(buff);
+        while (i != -1) {
+            outputStream.write(buff, 0, buff.length);
+            outputStream.flush();
+            i = bis.read(buff);
+        }
     }
 }
