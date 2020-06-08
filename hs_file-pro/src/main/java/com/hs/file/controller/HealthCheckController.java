adab8d44c4c4bf3fe290072491a984b4bdd72f0b
zouyunke <zou_yunke@hismartinfo.com>
2020-06-08 16:59:37 +0800

    使用validator + bindresult 检验参数

--- hs_file-pro/src/main/java/com/hs/file/controller/HealthCheckController.java
+++ hs_file-pro/src/main/java/com/hs/file/controller/HealthCheckController.java
@@ -1,11 +1,13 @@
 package com.hs.file.controller;
 
+import com.hs.file.entity.User;
 import com.zyk.common.entity.ResultVo;
 import com.zyk.common.enums.ResponseValues;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.Date;
 
 /**
@@ -38,5 +40,13 @@ public class HealthCheckController {
     @GetMapping
     public ResultVo checkHealth() {
         return ResultVo.INFO(ResponseValues.NO_PERSON,new Date());
+    }
+    @PostMapping("/user")
+    public String addUser(@RequestBody @Valid User user, BindingResult bindingResult) {
+        // 如果有参数校验失败，会将错误信息封装成对象组装在BindingResult里
+        for (ObjectError error : bindingResult.getAllErrors()) {
+            return error.getDefaultMessage();
+        }
+        return null;
     }
 }
---
+++ hs_file-pro/src/main/java/com/hs/file/entity/User.java
@@ -0,0 +1,39 @@
+package com.hs.file.entity;
+
+import javax.validation.constraints.NotNull;
+
+/**
+ * @author by Forrest
+ * @Classname User
+ * @Description 测试 validator + BindResult  打造优雅接口
+ * @Date 2020/6/8 16:51
+ * @Email comeonZYK@outlook.com
+ * ┏┛ ┻━━━━━┛ ┻┓
+ * ┃　　　　　　 ┃
+ * ┃　　　━　　　┃
+ * ┃　┳┛　  ┗┳　┃
+ * ┃　　　　　　 ┃
+ * ┃　　　┻　　　┃
+ * ┃　　　　　　 ┃
+ * ┗━┓　　　┏━━━┛
+ * ┃　　　┃   神兽保佑
+ * ┃　　　┃   代码无BUG！
+ * ┃　　　┗━━━━━━━━━┓
+ * ┃　　　　　　　    ┣┓
+ * ┃　　　　         ┏┛
+ * ┗━┓ ┓ ┏━━━┳ ┓ ┏━┛
+ * ┃ ┫ ┫   ┃ ┫ ┫
+ * ┗━┻━┛   ┗━┻━┛
+ */
+public class User {
+    @NotNull(message = "id 不能为空")
+    private String id;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+}
